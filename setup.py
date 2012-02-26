181e5c6c7ddc350b91e8fbed3bd0adb857c66dfe
Kies <lleelm@gmail.com>
2012-02-26 21:07:29 +0800

    add desc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='mlogging',
     version='0.1',
-    description='logging handler for the multiprocess',
+    description='file/time rotate logging handler for the multiprocess',
     author='Kies Lee',
     author_email='lleelm@gmail.com',
     packages=['mlogging'],
